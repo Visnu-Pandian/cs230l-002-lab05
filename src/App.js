a86f2d90b61cc7e3bcb59edcd82efe2f72c7bc16
Visnu-Pandian <vsp00002@mix.wvu.edu>
2023-10-24 20:32:31 -0400

    lab06 submission

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import './App.css';
 import Navbar from './Navbar';
 import Card from './Card';
 
+
 function App() {
   return (
-    <div className="App bg-dark text-white">
+    <div className="App">
       <Navbar />
       <h1>CS 230L</h1>
       <h2>Section-02</h2>
       <p>WVU ID: 800391317</p>
       <p>Hi, I am Visnu.</p>
-      <Card /> <Card /> <Card />
+      <Card />
     </div>
   );
 }
